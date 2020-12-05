e8158832e751962c236c0bccf0c5f3d8e8d1d734
harshshaw <harshshaw5@gmail.com>
2020-12-05 23:30:57 +0530

    feature:login and logout token userId

--- web/src/Auth/login/login.js
+++ web/src/Auth/login/login.js
@@ -6,48 +6,54 @@ import { signInWithGoogle } from "../firebaseConfig";
 import { auth } from "../firebaseConfig";
 
 const Login = () => {
-  const [email, updateEmail] = useState(null);
+    const [email, updateEmail] = useState(null);
 
-  var unsubscribeFromAuth = null;
+    var unsubscribeFromAuth = null;
 
-  const signin = async () => {
-    await signInWithGoogle();
-    unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      updateEmail(user.email);
-      axios
-        .post("https://jan-suvidha.herokuapp.com/api/v1/users/login", {
-          email: user.email,
-        })
-        .then((res) => console.log(res))
-        .catch((error) => console.log(error));
-    });
-  };
+    const signin = async () => {
+        await signInWithGoogle();
+        unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+            updateEmail(user.email);
+            axios
+                .post("https://jan-suvidha.herokuapp.com/api/v1/users/admin/login", {
+                    email: user.email,
+                    department: 'a'
+                })
+                .then((res) => {
+                    return (
+                        localStorage.setItem('token', res.data.token),
+                        localStorage.setItem('userId', res.data.userId)
+                    )
+                })
+                .catch((error) => console.log(error));
+        });
+    };
 
-  useEffect(() => {}, [email]);
+    useEffect(() => { }, [email]);
 
-  return (
-    <div className='user-info'>
-      {email ? (
-        <div>
-          <div>
-            <img src='xyz' />
-          </div>
-          <div>Name:</div>
-          <div>Email:</div>
+    return (
+        <div className='user-info'>
+            {email ? (
+                <div>
+                    <div>
+                        <img src='xyz' />
+                    </div>
+                    <div>Name:</div>
+                    <div>Email:</div>
 
-          <button
-            onClick={() => {
-              return auth.signOut(), updateEmail(null);
-            }}
-          >
-            LOG OUT
+                    <button
+                        onClick={() => {
+                            return auth.signOut(), localStorage.clear('auth'), localStorage.clear('userId'), updateEmail(null);
+                        }}
+                    >
+                        LOG OUT
           </button>
+                </div>
+            ) : (
+                    <button onClick={() => signin()}>SIGN IN WITH GOOGLE</button>
+                )}
         </div>
-      ) : (
-        <button onClick={() => signin()}>SIGN IN WITH GOOGLE</button>
-      )}
-    </div>
-  );
+    );
 };
 
 export default Login;
